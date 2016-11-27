545a279bd506fa6a2d1167bb0838ad51b3660614
Florian JUDITH <Florian JUDITH>
2016-11-27 08:39:26 +0100

    Simplified documentation #2

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-http://repo.squashtest.org/distribution/squash-tm-1.14.2.RELEASE.zip
+
 # International build
 docker build --build-arg=SQUASH_TM_LANGUAGE=en --tag=fjudith/squash-tm:1.14.2 .
-docker build --build-arg=SQUASH_TM_LANGUAGE=en --tag=fjudith/squash-tm .
+docker build --build-arg=SQUASH_TM_LANGUAGE=en --tag=fjudith/squash-tm:latest .
